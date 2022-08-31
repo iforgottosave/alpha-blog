3ad0f24d9a6a200e2a5306c6c8e0624bb5727d2c
Denis Chernyavsky <77507897+iforgottosave@users.noreply.github.com>
2022-08-30 18:45:24 -0700

    Added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  # helper_method enables helpers to access controllers
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # ||= if user not found at time of request, THEN...
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def logged_in?
+    # !! boolean or
+    !!current_user
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,4 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block" )
   end
 
-  def current_user
-    # ||= if user not found at time of request, THEN...
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
-  end
-
-  def logged_in?
-    # !! boolean or
-    !!current_user
-  end
-
 end
