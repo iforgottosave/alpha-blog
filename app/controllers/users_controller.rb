f6f28125cc781922a4dae63579fcb39db4cf8fbf
Denis Chernyavsky <77507897+iforgottosave@users.noreply.github.com>
2022-09-01 14:19:24 -0700

    Restrict actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 10)
@@ -44,6 +45,13 @@ class UsersController < ApplicationController
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "Permission denied"
+        redirect_to @user
+      end
     end
 
 end
