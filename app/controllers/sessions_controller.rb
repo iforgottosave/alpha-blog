f7dab1a9b9f3e0822e390ed741c1a0c27484cac5
Denis Chernyavsky <77507897+iforgottosave@users.noreply.github.com>
2022-09-01 13:00:52 -0700

    Restrict user actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Login successful!"
       redirect_to user
     else
-      flash.now[:alert] = "Login incorrect"
+      flash.now[:alert] = "Incorrect credentials"
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logout successful"
+    flash[:notice] = "You have successfully logged out"
     redirect_to root_path
   end
 
